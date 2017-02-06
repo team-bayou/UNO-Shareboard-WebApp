8a252a7b3ffc21f152b459d6aa896f1a699d134c
Parker <psprouse@uno.edu>
2017-02-06 13:04:09 -0600

    Made some major changes to the project to prepare it for being deployed to Heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './js/components/App';
+import NotFound from './js/NotFound';
 import './css/styles.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('container')
-);
+ReactDOM.render((
+  <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    <Route path="/" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+), document.getElementById('container'));
---
+++ src/js/NotFound.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          404 <small>Not Found :(</small>
+        </h1>
+      </div>
+    );
+  }
+}
