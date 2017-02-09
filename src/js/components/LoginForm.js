5e92937f9c41c84f232a45ed3d3af4b2a57e11ac
Parker <psprouse@uno.edu>
2017-02-09 14:55:07 -0600

    Added some logic skeleton to the login form

--- src/js/components/LoginForm.js
+++ src/js/components/LoginForm.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import '../../css/styles.css';
 
 export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('An account was entered: ' + this.state.email + ' : ' + this.state.password);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <fieldset className="uk-fieldset">
           <legend className="uk-legend">Login</legend>
           <div className="uk-margin">
-            <input className="uk-input" type="text" placeholder="E-mail" />
+            <input name="email" className="uk-input" type="text" placeholder="E-mail" value={this.state.email} onChange={this.handleInputChange} />
           </div>
           <div className="uk-margin">
-            <input className="uk-input" type="password" placeholder="Password" />
+            <input name="password" className="uk-input" type="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} />
           </div>
           <div className="uk-margin">
             <button className="uk-button uk-button-secondary uk-align-center login-btn" type="submit" value="Login">Login</button>
