6e2b6d057b8ea4f30218448d70c1ff656c084589
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2017-02-12 17:16:11 -0600

    Add home page template, Update routes, Remove warning from Advertisement"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import LandingPage from './js/pages/LandingPage';
+import Landing from './js/pages/LandingPage';
+import Home from './js/pages/HomePage';
 import Advertisements from './js/pages/AdvertisementsPage';
 import NotFound from './js/pages/NotFoundPage';
 import './css/styles.css';
 
 ReactDOM.render((
   <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
-    <Route path="/" component={LandingPage} />
+    <Route path="/" component={Landing} />
+    <Route path="home" component={Home} />
     <Route path="advertisements" component={Advertisements} />
     <Route path="*" component={NotFound} />
   </Router>
