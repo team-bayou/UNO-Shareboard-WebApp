ed62c70b77ae8af2761c0f67e69120538d89989f
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2017-02-15 20:07:49 -0600

    Add reviews page and review components, Update routes and styles

---
+++ src/js/components/Review.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import avatar from '../../media/images/avatar.jpg';
+
+export default class Review extends Component {
+  render(){
+    var routeToUser = "/users/" + this.props.review.reviewer.account_name;
+
+    return (
+      <div id={"review-" + this.props.review.id} className="review">
+        <article className="uk-comment uk-background-muted">
+            <header className="uk-comment-header uk-grid-medium uk-flex-middle" data-uk-grid>
+                <div className="uk-width-auto">
+                    <img className="uk-comment-avatar" src={avatar} width="80" height="80" alt="" />
+                </div>
+                <div className="uk-width-expand">
+                    <h4 className="review-author uk-comment-title uk-margin-remove"><a className="uk-link-reset" href={routeToUser}>{this.props.review.reviewer.account_name}</a></h4>
+                    <p className="review-time-published uk-margin-remove">{this.props.review.time_published}</p>
+                    <p className="review-rating uk-margin-remove">{this.getRating()}</p>
+                </div>
+            </header>
+            <div className="uk-comment-body">
+                <p>{this.props.review.comments}</p>
+            </div>
+        </article>
+      </div>
+    );
+  }
+
+  getRating(){
+    var rating = [];
+
+    for (var i = 0; i < parseInt(this.props.review.rating, 10); i++)
+      rating.push(<span key={i} className="uk-icon-button" data-uk-icon="icon: star"></span>);
+
+    return rating;
+  }
+}
