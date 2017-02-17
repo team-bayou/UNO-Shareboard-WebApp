e47e31423859eca59a29d576983e8af62455df7f
dwstroud <dwstroud@uno.edu>
2017-02-16 20:40:10 -0600

    add buy/sell button, advertiseent feed

---
+++ src/js/components/BuySeekButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class BuySeekButton extends Component {
+  render(){
+    return(
+      <p data-uk-margin>
+        <div>
+          <button className="uk-button uk-button-default uk-button-large uk-vertical-align uk-margin-small-right">Buy</button>
+        </div>
+      </p>
+    )
+  }
+}
---
+++ src/js/components/SellTradeButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class SellTradeButton extends Component {
+  render(){
+    return(
+      <p data-uk-margin>
+        <div>
+          <button className="uk-button uk-button-default uk-button-large uk-vertical-align-middle uk-margin-small-right">Sell</button>
+        </div>
+      </p>
+    )
+  }
+}
