8d16a40bbc1b1e5bb885ffdb302776f3df9e916a
Parker <psprouse@uno.edu>
2017-03-18 18:54:37 -0500

    Started filling out the new password form

--- src/js/components/ResetPasswordForm.js
+++ src/js/components/ResetPasswordForm.js
@@ -8,7 +8,7 @@ export default class ForgotPasswordForm extends Component {
   constructor(props) {
     super(props);
 
-    let pageNeedsLoading = true;
+    let pageNeedsLoading = false;
     this.propsValid = true;
 
     if (!props.email || !props.code || !validateEmail(props.email)) {
@@ -16,18 +16,22 @@ export default class ForgotPasswordForm extends Component {
       pageNeedsLoading = false;
     }
     else {
-      utilities.checkForUnverifiedEmail(props.email, function (exists) {
+      /*utilities.checkForUnverifiedEmail(props.email, function (exists) {
         this.propsValid = exists;
         this.setState({
           pageLoaded: true
         });
       }.bind(this));
+      */
     }
 
     this.emptyFields = false;
 
     this.inputValid = "uk-input";
     this.inputInvalid = "uk-input uk-form-danger";
+
+    this.passwordValid = true;
+    this.passwordMatch = true;
 
     this.state = {
       password: '',
@@ -44,6 +48,52 @@ export default class ForgotPasswordForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleInputChange(event) {
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.checkForEmptyFields();
+  }
+
+  resetErrors() {
+    this.passwordValid = true;
+    this.passwordMatch = true;
+    this.emptyFields = false;
+    this.setState({
+      passwordStyle: this.inputValid,
+      passwordConfirmStyle: this.inputValid
+    });
+  }
+
+  checkForExistingErrors() {
+    const ps = this.state.password !== "" && this.state.password.length < 6 ? this.inputInvalid : this.inputValid;
+    const pcs = this.state.passwordConfirm !== "" && this.state.password !== this.state.passwordConfirm ? this.inputInvalid : this.inputValid;
+    this.setState({
+      passwordStyle: ps,
+      passwordConfirmStyle: pcs
+    });
+
+    this.passwordValid = ps === this.inputValid;
+    this.passwordMatch = pcs === this.inputValid;
+  }
+
+  checkForEmptyFields() {
+    if (this.state.password === "" || this.state.passwordConfirm === "") {
+      this.emptyFields = true;
+
+      const ps = this.state.password === "" || this.state.password.length < 6 ? this.inputInvalid : this.inputValid;
+      const pcs = this.state.passwordConfirm === "" || this.state.password !== this.state.passwordConfirm ? this.inputInvalid : this.inputValid;
+      this.setState({
+        passwordStyle: ps,
+        passwordConfirmStyle: pcs
+      });
+    }
+    else {
+      this.emptyFields = false;
+    }
+  }
 
   render() {
 
@@ -89,9 +139,16 @@ export default class ForgotPasswordForm extends Component {
 
                 <div className="uk-margin">
                   <div className="uk-form-controls">
-                    <input name="verifycode" className={this.state.verifycodeStyle} type="text" placeholder="Verification Code (required)" value={this.state.verifycode} onChange={this.handleInputChange} />
+                    <input name="password" className={this.state.passwordStyle} type="password" placeholder="New Password" value={this.state.password} onChange={this.handleInputChange} />
                   </div>
-                  <label className="uk-form-label label-invalid" hidden={this.verificationCorrect}>Verification code is incorrect</label>
+                  <label className="uk-form-label label-invalid" hidden={this.passwordValid}>Password is too short (minimum 6 characters)</label>
+                </div>
+
+                <div className="uk-margin">
+                  <div className="uk-form-controls">
+                    <input name="passwordConfirm" className={this.state.passwordConfirmStyle} type="password" placeholder="New Password (Confirm)" value={this.state.passwordConfirm} onChange={this.handleInputChange} />
+                  </div>
+                  <label className="uk-form-label label-invalid" hidden={this.passwordMatch}>Passwords don't match</label>
                 </div>
 
                 <div className="uk-margin">
