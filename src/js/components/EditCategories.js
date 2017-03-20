500d1314f9c06f86f9a17e23ea4b3fb5a99bde81
Parker <psprouse@uno.edu>
2017-03-20 09:49:48 -0500

    Made the categories table with some nifty features. Not interactive just yet

--- src/js/components/EditCategories.js
+++ src/js/components/EditCategories.js
@@ -25,16 +25,39 @@ export default class EditCategories extends Component {
     if (this.state.categories) {
       cats = this.state.categories.map(
         cat =>
-        <div key={cat.id}>
-          <span style={{backgroundColor: cat.color}}>&nbsp;&nbsp;&nbsp;</span>
-          <span>{cat.title}</span><br />
-        </div>
+        <tr key={cat.id}>
+          <td><a href="#" className="uk-link-reset" data-uk-icon="icon: close"></a></td>
+          <td style={{backgroundColor: cat.color}}></td>
+          <td className="uk-text-nowrap">
+            <input className="uk-input uk-form-blank admin-edit-field" type="text" defaultValue={cat.title} />
+          </td>
+          <td className="uk-text-nowrap">
+            <input className="uk-input uk-form-blank admin-edit-field" type="text" defaultValue={cat.description} />
+          </td>
+        </tr>
       );
     }
 
     return (
-      <div>
-        {cats}
+      <div className="uk-overflow-auto">
+        <table className="uk-table uk-table-small uk-table-middle">
+          <thead>
+            <tr>
+              <th></th>
+              <th className="uk-text-center">Color</th>
+              <th className="uk-text-center">Title</th>
+              <th className="uk-text-center">Description</th>
+          </tr>
+          </thead>
+          <tbody>
+            {cats}
+            <tr>
+              <td colSpan="4">
+                <button className="uk-button uk-button-secondary uk-align-center landing-submit-btn" type="submit" value="Save Changes">Save Changes</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
