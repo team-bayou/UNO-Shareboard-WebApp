e72e3941dfe39ed97f784ea6170508f94f011e8b
Parker Sprouse <poker158149@gmail.com>
2017-03-27 00:15:34 -0500

    Began experimenting with a profile page layout

--- src/js/pages/ProfilePage.js
+++ src/js/pages/ProfilePage.js
@@ -5,6 +5,8 @@ const utils = require('../utility/utilities');
 const api = require('../utility/api');
 const constants = require('../utility/constants');
 
+import avatar from '../../media/images/avatar.jpg';
+
 export default class ProfilePage extends Component {
 
   constructor(props) {
@@ -33,18 +35,47 @@ export default class ProfilePage extends Component {
 
     else {
       return (
-        <div id="home" className="app">
+        <div id="profile">
           <AppHeader />
-          <div className="app-body uk-container">
+          <div className="app-body uk-container uk-text-break">
 
             <div className="uk-margin-medium-bottom">
               <h2 className="uk-heading-line uk-text-center"><span>Profile</span></h2>
             </div>
 
-            <div data-uk-grid>
-              <div className="uk-width-1-1 uk-text-break">
-                {JSON.stringify(this.state.user)}
-              </div>
+            <div className="ad-owner">
+              <article className="uk-comment">
+                <header className="uk-comment-header uk-grid-medium uk-flex-middle" data-uk-grid>
+                  <div className="uk-width-auto">
+                    <img className="ad-owner-image uk-comment-avatar" src={avatar} width="80" height="80" alt="" />
+                  </div>
+                  <div className="uk-width-expand">
+                    <ul className="ad-owner-contact uk-comment-meta uk-subnav uk-subnav-divider">
+                      <li>
+                        {this.state.user.accountName}
+                      </li>
+                      <li>
+                        {(this.state.user.firstName || "---") + " " + (this.state.user.lastName || "---")}
+                      </li>
+                      <li>
+                        {this.state.user.email}
+                      </li>
+                      <li>
+                        {this.state.user.phoneNumber}
+                      </li>
+                      <li>
+                        <a href={"https://www.facebook.com/" + this.state.user.facebookId} className="ad-owner-facebook" target="_blank">{this.state.user.facebookId || "No Facebook"}</a>
+                      </li>
+                      <li>
+                        <a href={"https://www.twitter.com/" + this.state.user.twitterHandle} className="ad-owner-twitter" target="_blank">{this.state.user.twitterHandle || "No Twitter"}</a>
+                      </li>
+                      <li>
+                        {"User Reviews (" + (!this.state.user.reviews ? "0" : this.state.user.reviews) + ")"}
+                      </li>
+                    </ul>
+                  </div>
+                </header>
+              </article>
             </div>
 
           </div>
