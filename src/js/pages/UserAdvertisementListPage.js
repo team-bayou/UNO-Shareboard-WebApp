3b256416fbeb70bf9816299e3c498e5a4ca74c8b
Parker <psprouse@uno.edu>
2017-03-29 14:04:18 -0500

    Updating user listings page to be dynamic based on whether it's yours or another user's

--- src/js/pages/UserAdvertisementListPage.js
+++ src/js/pages/UserAdvertisementListPage.js
@@ -1,4 +1,6 @@
 import api from '../utility/api';
+import utils from '../utility/utilities';
+import constants from '../utility/constants';
 
 import React, { Component } from 'react';
 import AppHeader from '../components/AppHeader';
@@ -10,37 +12,107 @@ export default class UserAdvertisementsPage extends Component {
     super(props);
 
     this.state = {
-      advertisements: []
+      advertisements: [],
+      user: null,
+      userExists: false,
+      myListings: false
     };
   }
 
   componentDidMount() {
-    let self = this;
-
     // Try to get a list of user's advertisements.
     api.getUserAdvertisements(this.props.params.id, function(exists, response){
       if (exists && response){
-        self.setState({
+        this.setState({
           advertisements: response.data
         });
       } else {
         console.log("No advertisements found");
       }
-    });
+    }.bind(this));
+
+    api.getUserByID(this.props.params.id, function(exists, response) {
+      if (exists) {
+        this.setState({
+          user: response.data,
+          userExists: true,
+          myListings: (this.props.params.id + "") === (utils.getCookie(constants.COOKIE_A) + "")
+        });
+      }
+      else {
+        this.setState({
+          user: -1,
+          userExists: false
+        });
+        console.log("User doesn't exist");
+      }
+    }.bind(this));
   }
 
   render() {
-    if (!this.state.advertisements)
+    if (!this.state.advertisements || !this.state.user)
       return (<div className="uk-text-center">Loading...</div>);
+
+    if (!this.state.userExists) {
+      return (
+        <div id="ad-list" className="app">
+          <AppHeader />
+          <div className="app-body uk-container">
+            <div className="uk-margin uk-text-center">
+              The requested user does not exist.
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    if (this.state.advertisements.length === 0)
+      return (
+      <div id="ad-list" className="app">
+        <AppHeader />
+        <div className="app-body uk-container">
+          <h2 className="uk-heading-line uk-text-center">
+            <span>
+              {
+                this.state.myListings ?
+                "Your Current Listings"
+                :
+                this.state.user.accountName + "'s Current Listings"
+              }
+            </span>
+          </h2>
+          <a href="/advertisements/add" className="button-success uk-button uk-button-large uk-width-1-4 uk-margin-large-bottom uk-align-center">Create New Advertisement</a>
+
+          <div className="uk-margin uk-text-center">
+            {
+              this.state.myListings ?
+              "You do not currently have any listings."
+              :
+              this.state.user.accountName + " does not currently have any listings."
+            }
+          </div>
+
+        </div>
+      </div>
+      );
 
     return (
       <div id="ad-list" className="app">
         <AppHeader />
         <div className="app-body uk-container">
-          <h2 className="uk-heading-line uk-text-center"><span>{"Your Current Listings (" + this.state.advertisements.length + ")"}</span></h2>
-          <div className="uk-width-1-4 uk-align-center uk-margin-large-bottom">
-            <CreateButton href={"/advertisements/add"} name={"Create advertisement"} />
-          </div>
+          <h2 className="uk-heading-line uk-text-center">
+            <span>
+              <span>
+                {
+                  (this.props.params.id + "") === (utils.getCookie(constants.COOKIE_A) + "") ?
+                  "Your Current Listings (" + this.state.advertisements.length + ")"
+                  :
+                  this.state.user.accountName + "'s Current Listings (" + this.state.advertisements.length + ")"
+                }
+              </span>
+            </span>
+          </h2>
+          <a href="/advertisements/add" className="button-success uk-button uk-button-large uk-width-1-4 uk-margin-large-bottom uk-align-center">Create New Advertisement</a>
           <AdList advertisements={this.state.advertisements}/>
         </div>
       </div>
