882390fe1e6130abfd5c24d1ff6373d409d6cbbd
Parker Sprouse <poker158149@gmail.com>
2017-04-04 23:10:06 -0500

    Made more progress on the form

--- src/js/components/ReportProblemForm.js
+++ src/js/components/ReportProblemForm.js
@@ -8,13 +8,24 @@ export default class LoginForm extends Component {
   constructor(props) {
     super(props);
 
+    this.emptyFields = false;
+    this.emailValid = true;
+
+    this.inputValid = "uk-input";
+    this.inputInvalid = "uk-input uk-form-danger";
+
+    this.textareaValid = "uk-textarea";
+    this.textareaInvalid = "uk-textarea uk-form-danger";
+
     this.state = {
       loaded: false,
-      user: null,
 
       name: "",
       email: "",
-      description: ""
+      description: "",
+
+      emailStyle: this.inputValid,
+      descriptionStyle: this.textareaValid
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -50,12 +61,50 @@ export default class LoginForm extends Component {
     const name = target.name;
 
     this.setState({
-      [name]: value,
+      [name]: value
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
+
+    this.resetErrors();
+    this.checkForEmptyFields();
+
+    if (!this.emptyFields) {
+      if (!utils.validateEmail(this.state.email)) {
+        this.emailValid = false;
+        this.setState({
+          emailStyle: this.inputInvalid
+        });
+      }
+      else {
+        console.log(this.state.description);
+      }
+    }
+  }
+
+  resetErrors() {
+    this.emptyFields = false;
+    this.emailValid = true;
+
+    this.setState({
+      emailStyle: this.inputValid,
+      descriptionStyle: this.textareaValid
+    });
+  }
+
+  checkForEmptyFields() {
+    if (this.state.email === "" || this.state.description === "") {
+      this.emptyFields = true;
+
+      const es = this.state.email === "" ? this.inputInvalid : this.inputValid;
+      const ds = this.state.description === "" ? this.textareaInvalid : this.textareaValid;
+      this.setState({
+        emailStyle: es,
+        descriptionStyle: ds
+      });
+    }
   }
 
   render() {
@@ -63,6 +112,8 @@ export default class LoginForm extends Component {
       return (
         <form className="uk-form-stacked" onSubmit={this.handleSubmit}>
           <fieldset className="uk-fieldset">
+
+            <label className="uk-form-label label-invalid" hidden={!this.emptyFields}>Please make sure all required fields are filled out</label>
 
             <div className="uk-margin">
               <label className="uk-form-label form-label" htmlFor="name">Your Name</label>
@@ -72,19 +123,18 @@ export default class LoginForm extends Component {
             </div>
 
             <div className="uk-margin">
-              <label className="uk-form-label form-label" htmlFor="email">Your E-mail *</label>
+              <label className="uk-form-label form-label" htmlFor="email">Your E-mail <span className="label-invalid">*</span></label>
               <div className="uk-form-controls">
-                <input name="email" id="email" className="uk-input" type="text" placeholder="Your E-mail" value={this.state.email} onChange={this.handleInputChange} />
+                <input name="email" id="email" className={this.state.emailStyle} type="text" placeholder="Your E-mail" value={this.state.email} onChange={this.handleInputChange} />
               </div>
-              <label className="uk-form-label label-invalid" hidden={true}>Please make sure your e-mail is filled out and valid</label>
+              <label className="uk-form-label label-invalid" hidden={this.emailValid}>Please make sure your e-mail is valid</label>
             </div>
 
             <div className="uk-margin">
-              <label className="uk-form-label form-label" htmlFor="description">Description of your problem *</label>
+              <label className="uk-form-label form-label" htmlFor="description">Description of your problem <span className="label-invalid">*</span></label>
               <div className="uk-form-controls">
-                <textarea name="description" id="description" className="uk-textarea" rows="5" placeholder="Description of your problem" onChange={this.handleInputChange} />
+                <textarea name="description" id="description" className={this.state.descriptionStyle} rows="5" placeholder="Description of your problem" onChange={this.handleInputChange} />
               </div>
-              <label className="uk-form-label label-invalid" hidden={true}>Please provide a description of your problem</label>
             </div>
 
             <div className="uk-margin">
@@ -92,7 +142,7 @@ export default class LoginForm extends Component {
             </div>
 
             <div className="uk-margin-top uk-text-center">
-              <a href="/">Back</a>
+              <a href="/">Home</a>
             </div>
 
           </fieldset>
