4ba3af3aa5fd49e27a268fd3555247522f7c7817
Parker Sprouse <poker158149@gmail.com>
2017-04-20 20:11:52 -0500

    A few missed lines

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -169,8 +169,8 @@ export default class NavBar extends Component {
                       <li className="uk-nav-header">Listings</li>
                       <li className="uk-parent">
                         <ul className="uk-nav-sub">
-                          <li><a href="/advertisements"><span className="uk-margin-small-right" data-uk-icon="icon: search"></span>Buy / Seek</a></li>
-                          <li><a href="/advertisements"><span className="uk-margin-small-right" data-uk-icon="icon: credit-card"></span>Sell / Offer</a></li>
+                          <li><a href="/advertisements/seek"><span className="uk-margin-small-right" data-uk-icon="icon: search"></span>Buy / Seek</a></li>
+                          <li><a href="/advertisements/offer"><span className="uk-margin-small-right" data-uk-icon="icon: credit-card"></span>Sell / Offer</a></li>
                           <li><a href={routeToUserAds}><span className="uk-margin-small-right" data-uk-icon="icon: bookmark"></span>My Listings</a></li>
                           <li><a href="/advertisements/add"><span className="uk-margin-small-right" data-uk-icon="icon: file-edit"></span>Create Listing</a></li>
                         </ul>
