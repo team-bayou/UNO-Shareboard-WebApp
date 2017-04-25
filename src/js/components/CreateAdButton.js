515b8367f53f42edb17cf861800dffc46187bf23
Parker Sprouse <poker158149@gmail.com>
2017-04-25 16:13:46 -0500

    Delete unnecessary button

--- src/js/components/CreateAdButton.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class CreateAdButton extends Component {
-  render(){
-    return(
-      <a href="/advertisements" className="uk-button uk-button-danger uk-button-large uk-margin-small-left uk-width-1-5">Create Advertisement</a>
-    )
-  }
-}
