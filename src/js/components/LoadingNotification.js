679168b00717c0b0016345b447e310e6ab5b6263
Parker Sprouse <poker158149@gmail.com>
2017-04-28 15:57:56 -0500

    Center the loading text on the page

--- src/js/components/LoadingNotification.js
+++ src/js/components/LoadingNotification.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class LoadingNotification extends Component {
   render() {
     return (
-      <div className="uk-text-center">Loading...</div>
+      <div className="uk-text-center center-page">Loading...</div>
     );
   }
 }
