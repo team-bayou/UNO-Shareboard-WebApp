b5ffddf824693b0e671047134e849ad0457b74ce
Parker Sprouse <poker158149@gmail.com>
2017-04-29 17:39:47 -0500

    Added a header to the landing pages that make them look much nicer

--- src/js/pages/NotFoundPage.js
+++ src/js/pages/NotFoundPage.js
@@ -5,6 +5,9 @@ export default class NotFound extends Component {
   render() {
     return (
       <div className="app">
+        <div className="app-header">
+          <h1 className="uk-heading uk-text-center"><a href="/" className="header-link">UNO Shareboard</a></h1>
+        </div>
         <div className="app-body uk-container">
           <div className="landing-form uk-card uk-card-default center">
             <div className="uk-card-body">
--- src/js/pages/ResetPasswordPage.js
+++ src/js/pages/ResetPasswordPage.js
@@ -7,6 +7,9 @@ export default class ForgotPasswordPage extends Component {
   render() {
     return (
       <div className="app">
+        <div className="app-header">
+          <h1 className="uk-heading uk-text-center"><a href="/" className="header-link">UNO Shareboard</a></h1>
+        </div>
         <div className="app-body uk-container">
           <div className="landing-form uk-card uk-card-default center">
             <div className="uk-card-body">
