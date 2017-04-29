606b2052f8c4c06b66d0715440cf1dc978e6d333
Parker Sprouse <poker158149@gmail.com>
2017-04-29 04:02:00 -0500

    Added a loading gif to make loading nicer

--- src/js/components/LoadingNotification.js
+++ src/js/components/LoadingNotification.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import loadingGIF from '../../media/images/hourglass.gif';
+
 export default class LoadingNotification extends Component {
   render() {
     return (
-      <div className="uk-text-center center-page">Loading...</div>
+      <div className="uk-text-center center-page">
+        <img src={loadingGIF} alt="Loading..." width="100" height="100" />
+      </div>
     );
   }
 }
